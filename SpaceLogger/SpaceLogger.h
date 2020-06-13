fc3b57ad90bb4a03faf0fcc37480551d0515c030
Dmitrii Chikovinskii <git@dmitrii.space>
2020-06-13 12:46:49 +0300

    Initial Commit

---
+++ SpaceLogger/SpaceLogger.h
@@ -0,0 +1,19 @@
+//
+//  SpaceLogger.h
+//  SpaceLogger
+//
+//  Created by Dmitrii Space on 13.06.2020.
+//  Copyright © 2020 Dmitrii Chikovinskii. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpaceLogger.
+FOUNDATION_EXPORT double SpaceLoggerVersionNumber;
+
+//! Project version string for SpaceLogger.
+FOUNDATION_EXPORT const unsigned char SpaceLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpaceLogger/PublicHeader.h>
+
+
